6924a33bdbb792ec8ad5996b2225d1101e10f74f
David Worth <dave@defenseunicorns.com>
2024-10-08 17:20:47 -0600

    feat(skeleton): create skeleton for the exercise

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+
+#include "main.h"
+
+using namespace std;
+int main(int argc, char *argv[], char *envp[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
